e2ce31114a54edce060b1d24fd5d3bc2cb60a3b9
be999 <be99999@likelion.org>
2021-07-26 16:21:27 +0900

    7/26 1ban class commit 4

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profileapp')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
-    image = models.ImageField(upload_to='profileapp/', null=True)
+    image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True)
     message = models.CharField(max_length=200, null=True)
 
